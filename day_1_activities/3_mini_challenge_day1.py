1ac7d20a967c7a02679a32e604bc9f636b1efce1
asanchez365365 <saintxotic123@gmail.com>
2025-12-17 00:56:31 +0000

    challenge

--- day_1_activities/3_mini_challenge_day1.py
+++ day_1_activities/3_mini_challenge_day1.py
@@ -37,24 +37,120 @@
     #    - Primary Email
     #    - Secondary Email
 
+students = []   # This is the main list of students
+#open brackets so I can use later
+
+cps_id = input("CPS ID: ").strip()                  # CPS ID
+first_name = input("First Name: ").strip().title()   # First Name
+last_name = input("Last Name: ").strip().title()     # Last Name
+middle_name = input("Middle Name: ").strip().title() # Middle Name
+homeroom = input("Homeroom: ").strip()               # Homeroom
+grade = input("Grade Level: ").strip()               # Grade Level
+primary_email = input("Primary Email: ").strip()     # Primary Email
+secondary_email = input("Secondary Email: ").strip() # 5Secondary Email
+
+
+
 # 2. Combine the First and Last name into this format:
     #    "Last, First"  
 
+full_name = f"{last_name}, {first_name}"
+
 # 3. Store all of the new information into ONE dictionary
     #    that matches the structure of the existing student data.
+new_student = {
+        "cps_id": cps_id,
+        "name": full_name,
+        "middle_name": middle_name,
+        "homeroom": homeroom,
+        "grade": grade,
+        "primary_email": primary_email,
+        "secondary_email": secondary_email
+    }
 
 # 4. Add (append) that new dictionary into the main students list.
+duplicate = False
+if not duplicate:
+    students.append(new_student)
 
 # 5. After adding the student, the program must:
     #    - Print a confirmation message
     #    - Print the total number of students in the system
     #    - Print the newly added student record
-
+print("\n Student successfully added!")                # 5.a Confirmation message
+print("Total number of students:", len(students))        # 5.b Total number of students
+print("New Student Record:")                              # 5.c Newly added student record
+print(new_student)
 # 6. The program must NOT delete or overwrite any existing students.
-
 # 7. If the CPS ID already exists in the system:
         #    - Do NOT add the student
         #    - Display an error message saying the CPS ID is already taken
 
+duplicate = any(student["cps_id"] == cps_id for student in students)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+students = []   # This is the main list of students
+#open brackets so I can use later
+
+cps_id = input("CPS ID: ").strip()                  # CPS ID
+first_name = input("First Name: ").strip().title()   # First Name
+last_name = input("Last Name: ").strip().title()     # Last Name
+middle_name = input("Middle Name: ").strip().title() # Middle Name
+homeroom = input("Homeroom: ").strip()               # Homeroom
+grade = input("Grade Level: ").strip()               # Grade Level
+primary_email = input("Primary Email: ").strip()     # Primary Email
+secondary_email = input("Secondary Email: ").strip() # 5Secondary Email
+
+
+
+# 2. Combine the First and Last name into this format:
+    #    "Last, First"  
+
+full_name = f"{last_name}, {first_name}"
+
+# 3. Store all of the new information into ONE dictionary
+    #    that matches the structure of the existing student data.
+new_student = {
+        "cps_id": cps_id,
+        "name": full_name,
+        "middle_name": middle_name,
+        "homeroom": homeroom,
+        "grade": grade,
+        "primary_email": primary_email,
+        "secondary_email": secondary_email
+    }
+
+# 4. Add (append) that new dictionary into the main students list.
+duplicate = False
+if not duplicate:
+    students.append(new_student)
+
+# 5. After adding the student, the program must:
+    #    - Print a confirmation message
+    #    - Print the total number of students in the system
+    #    - Print the newly added student record
+print("\n Student successfully added!")                # 5.a Confirmation message
+print("Total number of students:", len(students))        # 5.b Total number of students
+print("New Student Record:")                              # 5.c Newly added student record
+print(new_student)
+# 6. The program must NOT delete or overwrite any existing students.
+# 7. If the CPS ID already exists in the system:
+        #    - Do NOT add the student
+        #    - Display an error message saying the CPS ID is already taken
+
+duplicate = any(student["cps_id"] == cps_id for student in students)
 
